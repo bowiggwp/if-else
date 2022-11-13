b39b97c522c68f74500f140b863c8797b4de943d
bowiggwp <118131019+bowiggwp@users.noreply.github.com>
2022-11-13 21:53:15 +0300

    Add files via upload

---
+++ 10.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n;
+
+	setlocale(LC_ALL, "");
+
+	cout << "введите число: ";
+	cin >> n;
+
+	if (n < 10) {
+		cout << "это число меньше 10" << endl;
+	}
+	else if (n == 10)
+	{
+		cout << "это число равно 10" << endl;
+	}
+	else
+	{
+		cout << "это число больше 10" << endl;
+	}
+	return 0;
+}
